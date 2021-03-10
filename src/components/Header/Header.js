9c6d6eb2fd9e6df7c987220622e5fc9e9091c013
Naimul Alam <naimul2707@gmail.com>
2021-03-10 06:16:31 +0100

    font-awsome intalled and background img set

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Button, Container, Image } from 'react-bootstrap';
+import { Container, } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
     return (
         <Container fluid className='header'>
-            <h1 className='header-txt'>La Liga Teams</h1>
+            <h1 className='header-txt'>
+                <FontAwesomeIcon className='team-icon' icon={faUsers} />La Liga Teams
+            </h1>
         </Container>
     );
 };
